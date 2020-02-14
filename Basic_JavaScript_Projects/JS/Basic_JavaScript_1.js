cc9b89ef87671bd015353aaa5ec7862bd2b7fe9c
jaygahan <60200397+jaygahan@users.noreply.github.com>
2020-02-14 08:45:49 -0800

    Javascript Projects

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,28 @@
+
+
+document.write('Hello, world!<br>');
+
+var red ="This is a string<br>";
+var red = red.fontcolor("red");
+var green = "Also a string<br>";
+var green = green.fontcolor("green");
+var  orange= "Yet another String<br>";
+var orange = orange.fontcolor("orange");
+document.write (red);
+
+var blues = "This is blue.<br>";
+var blues= blues.fontcolor("blue");
+document.write(blues);
+
+
+
+
+
+var B ="It is raining!";
+        window.alert(B);
+
+document.write("\"Whether you think you can, or think you can't, you're right\" - Henry Ford<br>");    
+
+document.write(" It is raining"+" so I am staying inside"+" and watching TV<br>");
+
+document.write(5+4);
