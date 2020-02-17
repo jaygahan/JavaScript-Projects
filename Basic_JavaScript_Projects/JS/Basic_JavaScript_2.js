e01622eb15a52a9c101dd65523e1288343b78545
jaygahan <60200397+jaygahan@users.noreply.github.com>
2020-02-16 23:02:50 -0800

    Javascript Push
    Step 85

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
@@ -14,3 +14,8 @@ function myFunction() {
     sentence += "sentence";
     document.getElementById("words").innerHTML = sentence;
 }
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+}
+document.getElementById("demo").innerHTML = toCelsius(77);
